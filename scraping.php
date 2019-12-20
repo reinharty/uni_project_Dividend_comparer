b826196dca26cfab51c6492784a3af696f6abad1
Yorrick <yreinhar@students.uni-mainz.de>
2019-12-20 17:07:45 +0100

    Dividenden pro jahr hinzugefuegt

--- scraping.php
+++ scraping.php
@@ -7,10 +7,10 @@ $period2 = 1;
 $historyA = array();
 $dividendA = array();
 
-function getCSV()
+function getCSV($url)
 {
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://query1.finance.yahoo.com/v7/finance/download/SKT?period1=738540000&period2=1576796400&interval=1mo&events=history&crumb=zqM3WbGRGZX');
+    curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $resp = curl_exec($ch);
@@ -32,5 +32,28 @@ function CSVToArray($resp){
     }
     return ($array);
 }
+
+//@TODO Datenbank anbinden
+function payedDividens($year){
+    $sum=0;//summiert die in dem Jahr bisher tatsaechlich bezahlten Dividenden
+    $counter=0;//anzahl der bezahlten Dividenden
+
+    $dividendA=CSVToArray(getCSV("https://query1.finance.yahoo.com/v7/finance/download/SKT?period1=738540000&period2=1576796400&interval=1mo&events=div&crumb=UO48Nwtc0Va"));
+
+    for($i=1; $i<count($dividendA)-1;$i++){
+        if(strpos($dividendA[$i][0],$year)>-1){
+            //echo "true"."\n";
+            $sum+=$dividendA[$i][1];
+            $counter+=1;
+        } else {
+            //echo "false"."\n";
+        }
+    }
+
+    echo "Sum: ".$sum." Payouts: ".$counter;
+}
+
+payedDividens("2009");
+
 
 ?>
